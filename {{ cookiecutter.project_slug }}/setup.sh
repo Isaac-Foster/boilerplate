1d5650d0eea394fa60a017337fe65daede216f26
Isaac-Foster <isack200961@hotmail.com>
2025-10-29 14:07:57 -0300

    chore: new folder tests with test sample bypass commit and new commands in setup.sh

--- {{ cookiecutter.project_slug }}/setup.sh
+++ {{ cookiecutter.project_slug }}/setup.sh
@@ -2,4 +2,7 @@ uv sync
 git init
 git config user.name "{{ cookiecutter.author_name }}" 
 git config user.email "{{ cookiecutter.email }}"
+git add tests .gitignore pyproject.toml README.md setup.sh config .github
+git commit -m "first commit"
+git branch -M main
 uv run pre-commit install
